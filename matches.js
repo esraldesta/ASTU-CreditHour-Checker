85124d0971967bd6e20129fbb0212dd80449d50e
esral-desta <esraldesta@gmail.com>
2023-03-22 21:48:34 +0300

    firefox basics

---
+++ matches.js
@@ -0,0 +1,181 @@
+﻿let button = document.createElement('input');
+let textInput = document.createElement('input');
+textInput.placeholder = "Course Code ex: CSE"
+let searchResultText = document.createElement("p")
+let form = document.createElement("div")
+
+textInput.type = "text"
+textInput.classList.add("myinput")
+button.type = 'button';
+button.value = 'Submit';
+button.classList.add("mybutton")
+form.style.position = 'fixed';
+form.style.top = '100px';
+form.style.right = '100px';
+form.style.zIndex = "1";
+form.id ="target"
+form.classList.add("mycontainer")
+form.classList.add("mytarget")
+
+
+
+
+let child = document.getElementsByClassName("card-body")[6]
+let transcript = document.getElementById("transcript")
+form.appendChild(textInput)
+form.appendChild(button)
+transcript.appendChild(form);
+
+button.addEventListener('click', button_click);
+
+
+
+
+const select = document.createElement('select');
+select.classList.add("myinput")
+select.setAttribute('id', 'dep');
+select.setAttribute('name', 'dep');
+
+
+
+const option0 = document.createElement('option');
+option0.setAttribute('value', '');
+option0.disabled = true
+option0.selected = true
+option0.appendChild(document.createTextNode('Select Dep'));
+
+const option1 = document.createElement('option');
+option1.setAttribute('value', 'cse');
+option1.appendChild(document.createTextNode('CSE'));
+
+const option2 = document.createElement('option');
+option2.setAttribute('value', 'communication');
+option2.appendChild(document.createTextNode('Communication'));
+
+const option3 = document.createElement('option');
+option3.setAttribute('value', 'power');
+option3.appendChild(document.createTextNode('Power'));
+
+select.appendChild(option0);
+select.appendChild(option1);
+select.appendChild(option2);
+select.appendChild(option3);
+
+form.appendChild(select);
+
+let info = document.createElement("div")
+info.style.marginTop = "10px";
+
+form.appendChild(info)
+
+
+
+
+select.addEventListener("change",(e)=>{
+
+  let creditHours = {};
+  let tables = document.querySelectorAll('div.mb-4 table.table.table-striped.text-nowrap');
+  for (let i = 0; i < tables.length; i++) {
+    let table = tables[i];
+  
+    // Loop through each row in the table (skip the header and footer rows)
+    for (let j = 1; j < table.rows.length - 2; j++) {
+      let row = table.rows[j];
+  
+      // Extract the course code and credit hour values
+      let code = row.cells[0].textContent;
+      let creditHour = parseInt(row.cells[2].textContent);
+  
+      // Add the code and credit hour to the dictionary
+      creditHours[code] = creditHour;
+    }
+  }
+
+
+
+
+  if(e.target.value === "cse"){
+    textInput.value = ""
+    allcourcescredithour = courseSummation("",creditHours)
+    couresStartsWithCSE = courseSummation("CSE",creditHours)
+    let p1 = document.createElement("p")
+    p1.innerText = "your total credit hour is " + allcourcescredithour + "/170"
+    let p2 = document.createElement("p")
+    p2.innerText = "your total MAJOR-COURSE credit hour is " + couresStartsWithCSE + "/102"
+    const infoDep = document.createElement("div")
+    infoDep.appendChild(p1)
+    infoDep.appendChild(p2)
+    info.replaceChildren(infoDep)
+
+  }
+  else if(e.target.value === "power"){
+    let p1 = document.createElement("p")
+    p1.innerText = "sorry!! service is not available for power students yet. but Soon!"
+    const infoDep = document.createElement("div")
+    infoDep.appendChild(p1)
+    info.replaceChildren(infoDep)
+
+  }
+  else if(e.target.value === "communication"){
+    let p1 = document.createElement("p")
+    p1.innerText = "sorry!! service is not available for communication students yet. but Soon!"
+    const infoDep = document.createElement("div")
+    infoDep.appendChild(p1)
+    info.replaceChildren(infoDep)
+  }
+
+})
+// select.onchange = 
+function button_click(){
+  let creditHours = {};
+  let tables = document.querySelectorAll('div.mb-4 table.table.table-striped.text-nowrap');
+  for (let i = 0; i < tables.length; i++) {
+    let table = tables[i];
+  
+    // Loop through each row in the table (skip the header and footer rows)
+    for (let j = 1; j < table.rows.length - 2; j++) {
+      let row = table.rows[j];
+  
+      // Extract the course code and credit hour values
+      let code = row.cells[0].textContent;
+      let creditHour = parseInt(row.cells[2].textContent);
+  
+      // Add the code and credit hour to the dictionary
+      creditHours[code] = creditHour;
+    }
+  }
+
+
+
+  const inputCourseCode = textInput.value.toUpperCase()
+  if (inputCourseCode){
+
+    searchResultText.innerText = "Total credit hour starting with " + inputCourseCode + " is: " + courseSummation(inputCourseCode,creditHours)
+  }else{
+    searchResultText.innerText = "please provide any Course code"
+  }
+  // form.appendChild(searchResultText)
+
+  info.replaceChildren(searchResultText)
+
+}
+
+
+function courseSummation(courseCode="",creditHours){
+  let sum = 0;
+  for (const course in creditHours) {
+    if (course.startsWith(courseCode)) {
+      sum += creditHours[course];
+    }
+  }
+  return sum
+}
+
+
+const sheet = new CSSStyleSheet();
+// sheet.replaceSync('.mytarget {background-color: red;}');
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,31 @@
+// let creditHours = {"ddd":12};
+// let tables;
+// window.onload=()=>{
+//     console.log("start");
+// // Select all tables in the webpage
+// // let tables = document.querySelectorAll('table.table.table-striped.text-nowrap');
+// // let tables = document.querySelectorAll('.card-body .table.table-striped');
+// tables = document.querySelectorAll('div.mb-4 table.table.table-striped.text-nowrap');
+// // Loop through each table
+// for (let i = 0; i < tables.length; i++) {
+//   let table = tables[i];
+
+//   // Loop through each row in the table (skip the header and footer rows)
+//   for (let j = 1; j < table.rows.length - 2; j++) {
+//     let row = table.rows[j];
+
+//     // Extract the course code and credit hour values
+//     let code = row.cells[0].textContent;
+//     let creditHour = parseInt(row.cells[2].textContent);
+
+//     // Add the code and credit hour to the dictionary
+//     creditHours[code] = creditHour;
+//   }
+// }
+//     console.log("finish");
+//     console.log(creditHours);
+// }
+
+// document.getElementById("id_save").onclick=()=>{
+//     console.log(document.getElementById("courseCode").value);
+// }
